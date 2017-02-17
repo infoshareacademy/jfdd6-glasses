2cd611dd13ad6bd126087f03e373b9f0a71e1bef
natan-mlk <adam.milko@gmail.com>
2017-02-17 20:19:16 +0100

    FD6GL-23_Avatars - avatars rotating all together when seen

--- js/avatar-animation.js
+++ js/avatar-animation.js
@@ -1,9 +1,9 @@
 $(window).on('scroll', function () {
 
-  var p = $('#container_section_3');
+  var p = $('.flip-container');
   var offset = p.offset().top;
   var scrollTop = $(window).scrollTop()
   if (scrollTop > (offset - window.outerHeight/2)) {
-    $( '.person' ).addClass("person-back");
+    $( '.flipper' ).addClass("rot");
   }
 });

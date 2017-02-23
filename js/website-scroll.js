77dd933fe7791c2030e987f1b2e87133b82f4f80
mitroc <michaltrocha@gmail.com>
2017-02-23 16:56:39 +0100

    Other- Work in progress.

--- js/website-scroll.js
+++ js/website-scroll.js
@@ -1,52 +1,15 @@
-/**
- * Created by mtr on 2017-02-16.
- */
+$('a[href^="#"]').on('click',function (event) {
+  var hash = this.hash;
+  var $target = $(hash);
+  var $topOffset = 0;
 
-$(document).ready(function(){
-  $('a[href^="#"]').on('click',function (event) {
-    var hash = this.hash;
-    var $target = $(hash);
-    var $topOffset = 0;
+  event.preventDefault();
 
-    event.preventDefault();
-
-    $('html, body').stop().animate(
-      {'scrollTop': $target.offset().top - $topOffset},
-      900,
-      'swing',
-      function () {
-          window.location.hash = hash;
-      });
-  });
+  $('html, body').stop().animate(
+    {'scrollTop': $target.offset().top - $topOffset},
+    900,
+    'swing',
+    function () {
+        window.location.hash = hash;
+    });
 });
-
-/*
-* Attribute starts with selector [name^=”value”]-------------------
-* Selects elements that have the specified
-* attribute with a value beginning exactly with a given string.
-*
-* event.preventDefault()-------------------
-* If this method is called,
-* the default action of the event will not be triggered.
-* For example, clicked anchors will not take the browser to a new URL.
-*
-* hash-----------------
-* hash returns the part of the URL that follows
-* the # symbol, including the # symbol.
-*
-* stop-------------------------
-* Stop the currently-running animation on the matched elements.
-*
-* animate-----------------------
-* .animate( properties [, duration ] [, easing ] [, complete ] )
-*
-* .offset() --------------------
-* Get the current coordinates of the first element, or set the
-* coordinates of every element, in the set of matched
-* elements, relative to the document.
-*
-* [complete] ----------------
-* A function to call once the animation is complete,
-* called once per matched element.
-* window.location.hash = anchor - visible in URL
-* */

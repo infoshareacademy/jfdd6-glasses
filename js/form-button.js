2eba603253b7b4905f40af6dfaf2888427d5c1c8
Sylwia Rzedzicka <rzedzicka@gmail.com>
2017-02-23 21:41:51 +0100

    FD6GL-26 add mail validation

--- js/form-button.js
+++ js/form-button.js
@@ -2,9 +2,28 @@
  * Created by zemr on 23.02.17.
  */
 
+var $emailError = $('.email-error');
 
 $('.enrollment-form').submit(function(event) {
-  $('.section2').css("display", "none");
-  $('.game').css("display", "flex");
   event.preventDefault();
+  var $email = $('input[name="email"]').val();
+
+  if(!validate($email)) {
+    if ($emailError) { $('.email-error').remove(); }
+    $('#info').append($('<p class="email-error">').text("Podany adres mailowy jest niepoprawny."));
+  } else {
+    $('.section2').css("display", "none");
+    $('.game').css("display", "flex");
+  }
 });
+
+function validate(email) {
+  var pattern = /^\S+@\S+[\.][0-9a-z]+$/;
+  return pattern.test(email);
+}
+
+if ($emailError) {
+  $('input[name="email"]').focusin(function() {
+    $('.email-error').remove();
+  });
+}
